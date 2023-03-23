149954a99d12e3b80cbb25fc49ad45c648f29084
AmadAliKhann <amadalikhan@live.com>
2023-03-23 14:08:13 +0100

    update

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 import express from 'express';
 import cors from 'cors';
-import { Low, JSONFile } from 'lowdb';
+import { Low} from 'lowdb';
+import {JSONFile} from 'lowdb/node'
 import { userRouter } from './routers/users.router.js';
 import { mainErrorHandler, noRouteHandler } from './middlewares/error.handler.js';
 import { todoRouter } from './routers/todos.router.js';
+import dotenv from 'dotenv';
 
 
 
 let app = express();
+dotenv.config();
 
 
 const adapter = new JSONFile('db.json');
 export const db = new Low(adapter);
 await db.read();
 
-db.data ||= { users: [], todos:[] };
+db.data = db.data || { users: [], todos:[] };
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -26,5 +29,5 @@ app.use('/todos', todoRouter)
 
 app.use(noRouteHandler)
 app.use(mainErrorHandler)
-const port = 5000;
+const port = process.env.PORT;
 app.listen(port, console.log(`server is up on port: ${port}. 👻`))
